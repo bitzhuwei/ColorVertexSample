5e7cf6a9de63a12999cd3ffee718c7f319d5cb71
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-04-24 00:41:27 +0800

    change axis size by invoking SetAxisSize

--- source/SharpGL/Samples/WinForms/ColorVertexSample/AxiesSceneControl.cs
+++ source/SharpGL/Samples/WinForms/ColorVertexSample/AxiesSceneControl.cs
@@ -15,8 +15,13 @@ namespace ColorVertexSample
     public class AxiesSceneControl : SceneControl
     {
         private Scene axisScene = new Scene();
-        private Size axisSize = new Size(80, 80);
         private ArcBallEffect2 rotationEffect;
+        private Bitmap bmpAxis = new Bitmap(80, 80);
+
+        public void SetAxisSize(int width, int height)
+        {
+            this.bmpAxis = new Bitmap(width, height);
+        }
 
         protected override void OnLoad(EventArgs e)
         {
@@ -24,7 +29,7 @@ namespace ColorVertexSample
 
             CreateOpenGL(axisScene);
 
-            this.InitAxisScene(this.axisScene);
+            this.InitAxis(this.axisScene);
 
             this.MouseDown += AxiesSceneControl_MouseDown;
             this.MouseMove += AxiesSceneControl_MouseMove;
@@ -32,49 +37,39 @@ namespace ColorVertexSample
             this.GDIDraw += AxiesSceneControl_GDIDraw;
         }
 
-        System.Drawing.Drawing2D.Matrix flip = new System.Drawing.Drawing2D.Matrix(1, 0, 0, -1, 0, 0);
+
         void AxiesSceneControl_GDIDraw(object sender, RenderEventArgs args)
         {
-            var g = args.Graphics;
-            //set the coordinate orignate to bottom left.
-
-            //transform and flip y axis of the control world coordinate system.
-            g.TranslateTransform(0, this.Height);
-            g.MultiplyTransform(flip);
-
-            //	Blit our offscreen bitmap.
-            using (var imgAxies = new Bitmap(this.axisSize.Width, this.axisSize.Height))
+            using (var graphics = Graphics.FromImage(bmpAxis))
             {
-                using (var gImage = Graphics.FromImage(imgAxies))
-                {
-                    this.axisScene.OpenGL.MakeCurrent();
-                    this.axisScene.Draw();
-                    var hdc = gImage.GetHdc();
-                    this.axisScene.OpenGL.Blit(hdc);
-                    gImage.ReleaseHdc(hdc);
-                }
-                g.DrawImage(imgAxies, 0, 0);
+                this.axisScene.OpenGL.MakeCurrent();
+                this.axisScene.Draw();
+                var handleDeviceContext = graphics.GetHdc();
+                this.axisScene.OpenGL.Blit(handleDeviceContext);
+                graphics.ReleaseHdc(handleDeviceContext);
             }
+
+            args.Graphics.DrawImage(bmpAxis, 0, this.Height - bmpAxis.Height);
         }
 
         private void CreateOpenGL(Scene scene)
         {
             OpenGL gl = new OpenGL();
-            gl.Create(this.OpenGLVersion, this.renderContextType, axisSize.Width, axisSize.Height, 32, null);
+            gl.Create(this.OpenGLVersion, this.renderContextType, 
+                bmpAxis.Width, bmpAxis.Height, 32, null);
             //  Set the most basic OpenGL styles.
             gl.ShadeModel(OpenGL.GL_SMOOTH);
             gl.ClearColor(0.0f, 0.0f, 0.0f, 0.0f);
             gl.ClearDepth(1.0f);
-
             gl.Enable(OpenGL.GL_DEPTH_TEST);
             gl.DepthFunc(OpenGL.GL_LEQUAL);
             gl.Hint(OpenGL.GL_PERSPECTIVE_CORRECTION_HINT, OpenGL.GL_NICEST);
-            gl.SetDimensions(axisSize.Width, axisSize.Height);
-            gl.Viewport(0, 0, axisSize.Width, axisSize.Height);
+            gl.SetDimensions(bmpAxis.Width, bmpAxis.Height);
+            gl.Viewport(0, 0, bmpAxis.Width, bmpAxis.Height);
             scene.OpenGL = gl; 
         }
 
-        private void InitAxisScene(SharpGL.SceneGraph.Scene scene)
+        private void InitAxis(SharpGL.SceneGraph.Scene scene)
         {
             scene.ClearColor = Color.Gold;
             //  Create the 'Look At' camera
@@ -140,31 +135,6 @@ namespace ColorVertexSample
                 this.rotationEffect.ArcBall.SetBounds(this.Width, this.Height);
                 this.rotationEffect.ArcBall.MouseDown(e.X, e.Y);
                 this.Invalidate();
-            }
-        }
-
-        private void OnPaintAxies(System.Windows.Forms.PaintEventArgs e)
-        {
-            //set the coordinate orignate to bottom left.
-
-            ////transform and flip y axis of the control world coordinate system.
-            //e.Graphics.TranslateTransform(0, this.Height);
-            //System.Drawing.Drawing2D.Matrix flip = new System.Drawing.Drawing2D.Matrix(1, 0, 0, -1, 0, 0);
-            //e.Graphics.MultiplyTransform(flip);
-
-            //	Blit our offscreen bitmap.
-            //using (var imgAxies = new Bitmap(this.axisArea.Width, this.axisArea.Height))
-            {
-                //using (var g = Graphics.FromImage(imgAxies))
-                var g = e.Graphics;
-                {
-                    this.axisScene.OpenGL.MakeCurrent();
-                    this.axisScene.Draw();
-                    var hdc = g.GetHdc();
-                    this.axisScene.OpenGL.Blit(hdc);
-                    g.ReleaseHdc(hdc);
-                }
-                //e.Graphics.DrawImage(imgAxies, 0, 0);
             }
         }
 
