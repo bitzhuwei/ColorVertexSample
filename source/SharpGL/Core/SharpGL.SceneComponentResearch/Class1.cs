c3eb1a9c8955383ebeb2cf3a5486b4169ea03821
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-04-30 21:41:36 +0800

    add library project 'SharpGL.SceneComponentResearch' to storesclasses unused in project 'SharpGL.SceneComponent'.

---
+++ source/SharpGL/Core/SharpGL.SceneComponentResearch/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpGL.SceneComponentResearch
+{
+    /// <summary>
+    /// This class library stores classes unused in <see cref="SharpGL.SceneComponent"/>
+    /// </summary>
+    public class Class1
+    {
+    }
+}
