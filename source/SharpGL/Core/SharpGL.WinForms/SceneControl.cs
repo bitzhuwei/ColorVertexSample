e8585b3efeb6b5ba1a298640c8d1bc9348ae5b04
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-04-30 18:18:04 +0800

    reset code for Light.cs, Scene.cs, SceneControl.cs to SharpGL's original code.

--- source/SharpGL/Core/SharpGL.WinForms/SceneControl.cs
+++ source/SharpGL/Core/SharpGL.WinForms/SceneControl.cs
@@ -5,71 +5,68 @@ using SharpGL.SceneGraph;
 
 namespace SharpGL
 {
-   	/// <summary>
-	/// The SceneControl is an OpenGLControl that contains and draws a Scene object.
+    /// <summary>
+    /// The SceneControl is an OpenGLControl that contains and draws a Scene object.
     /// </summary>
     [System.Drawing.ToolboxBitmap(typeof(SceneControl), "SharpGL.png")]
-	public class SceneControl : SharpGL.OpenGLControl
-	{
-		private System.ComponentModel.IContainer components = null;
+    public class SceneControl : SharpGL.OpenGLControl
+    {
+        private System.ComponentModel.IContainer components = null;
 
         public SceneControl()
-		{
-			// This call is required by the Windows.Forms Form Designer.
-			InitializeComponent();
+        {
+            // This call is required by the Windows.Forms Form Designer.
+            InitializeComponent();
 
-			SetStyle(ControlStyles.AllPaintingInWmPaint, true);
-			SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.UserPaint, true);
 
             //  Initialise the scene.
             SceneGraph.Helpers.SceneHelper.InitialiseModelingScene(Scene);
-		}
+        }
 
-		/// <summary> 
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if(components != null)
-				{
-					components.Dispose();
-				}
-			}
-			base.Dispose( disposing );
-		}
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                if (components != null)
+                {
+                    components.Dispose();
+                }
+            }
+            base.Dispose(disposing);
+        }
 
-		#region Component Designer generated code
-		/// <summary> 
-		/// Required method for Designer support - do not modify 
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			// 
-			// OpenGLCtrl
-			// 
-			this.Name = "OpenGLCtrl";
+        #region Component Designer generated code
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            // 
+            // OpenGLCtrl
+            // 
+            this.Name = "OpenGLCtrl";
 
-            // force 'glA' pointing to 'glB'
-            // check https://github.com/dwmkerr/sharpgl/issues/105
-            this.scene.OpenGL = this.OpenGL;
-		}
-		#endregion
+        }
+        #endregion
 
-		protected override void OnPaint(PaintEventArgs e)
+        protected override void OnPaint(PaintEventArgs e)
         {
             //  Start the stopwatch so that we can time the rendering.
             stopwatch.Restart();
-            
-			//	Make sure it's our instance of openSharpGL that's active.
-			OpenGL.MakeCurrent();
 
-			//	Do the scene drawing.
-			scene.Draw();
+            //	Make sure it's our instance of openSharpGL that's active.
+            OpenGL.MakeCurrent();
 
-			//	If there is a draw handler, then call it.
+            //	Do the scene drawing.
+            scene.Draw();
+
+            //	If there is a draw handler, then call it.
             DoOpenGLDraw(new RenderEventArgs(e.Graphics));
 
             //  Draw the FPS.
@@ -92,46 +89,45 @@ namespace SharpGL
             stopwatch.Stop();
 
             //  Store the frame time.
-            frameTime = stopwatch.Elapsed.TotalMilliseconds;   
-		}
+            frameTime = stopwatch.Elapsed.TotalMilliseconds;
+        }
 
 
         /// <summary>
         /// Raises the <see cref="E:PaintBackground"/> event.
         /// </summary>
         /// <param name="e">The <see cref="System.Windows.Forms.PaintEventArgs"/> instance containing the event data.</param>
-		protected override void OnPaintBackground(PaintEventArgs e)
-		{
-			//	We override this, and don't call the base, i.e we don't paint
-			//	the background.
-		}
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //	We override this, and don't call the base, i.e we don't paint
+            //	the background.
+        }
 
         /// <summary>
         /// Raises the <see cref="E:System.Windows.Forms.Control.SizeChanged"/> event.
         /// </summary>
         /// <param name="e">An <see cref="T:System.EventArgs"/> that contains the event data.</param>
-		protected override void OnSizeChanged(EventArgs e)
-		{
-			//  Don't call the base- we handle sizing ourselves.
-            base.OnSizeChanged(e);
+        protected override void OnSizeChanged(EventArgs e)
+        {
+            //  Don't call the base- we handle sizing ourselves.
 
-			//	OpenGL needs to resize the viewport.
+            //	OpenGL needs to resize the viewport.
             OpenGL.SetDimensions(Width, Height);
-			scene.Resize(Width, Height);
+            scene.Resize(Width, Height);
 
-			Invalidate();
-		}
-                		
-		/// <summary>
-		/// This is the scene itself.
-		/// </summary>
-		private Scene scene = new Scene();
-        
-		[DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
-		public Scene Scene 
-		{
-			get {return scene;}
-			protected set {scene = value;}
-		}
-	}
+            Invalidate();
+        }
+
+        /// <summary>
+        /// This is the scene itself.
+        /// </summary>
+        private Scene scene = new Scene();
+
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public Scene Scene
+        {
+            get { return scene; }
+            set { scene = value; }
+        }
+    }
 }
