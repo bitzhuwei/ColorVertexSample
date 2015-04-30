7f8bb7135a9608ec57ed94db2e6446920277a912
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-04-30 12:11:50 +0800

    use FormFreeCamera

--- source/SharpGL/Samples/WinForms/ColorVertexSample/Program.cs
+++ source/SharpGL/Samples/WinForms/ColorVertexSample/Program.cs
@@ -13,10 +13,17 @@ namespace ColorVertexSample
         [STAThread]
         static void Main()
         {
+            Application.ThreadException += Application_ThreadException;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //Application.Run(new MainView());
-            Application.Run(new FormFixedCamera());
+            //Application.Run(new FormFixedCamera());
+            Application.Run(new FormFreeCamera());
+        }
+
+        private static void Application_ThreadException(object sender, System.Threading.ThreadExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.ToString());
         }
     }
 }
