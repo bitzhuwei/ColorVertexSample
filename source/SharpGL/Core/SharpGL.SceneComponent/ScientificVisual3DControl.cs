69d5ab87c5dc533ae285c82f5a96f28fe3baf426
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-01 14:56:30 +0800

    create UIScene for UI rendering in ScientificVisual3DControl

--- source/SharpGL/Core/SharpGL.SceneComponent/ScientificVisual3DControl.cs
+++ source/SharpGL/Core/SharpGL.SceneComponent/ScientificVisual3DControl.cs
@@ -17,5 +17,51 @@ namespace SharpGL.SceneComponent
     /// </summary>
     public partial class ScientificVisual3DControl : MySceneControl, ISupportInitialize
     {
+        public ScientificVisual3DControl()
+        {
+            this.UIScene = new MyScene();
+            this.UIScene.OpenGL = this.OpenGL;
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            //  Start the stopwatch so that we can time the rendering.
+            stopwatch.Restart();
+
+            //	Make sure it's our instance of openSharpGL that's active.
+            OpenGL.MakeCurrent();
+
+            //	Do the scene drawing.
+            Scene.Draw();
+
+            UIScene.Draw();
+
+            //	If there is a draw handler, then call it.
+            DoOpenGLDraw(new RenderEventArgs(e.Graphics));
+
+            //  Draw the FPS.
+            if (DrawFPS)
+            {
+                OpenGL.DrawText(5, 5, 1.0f, 0.0f, 0.0f, "Courier New", 12.0f,
+                    string.Format("Draw Time: {0:0.0000} ms ~ {1:0.0} FPS", frameTime, 1000.0 / frameTime));
+                OpenGL.Flush();
+            }
+
+            //	Blit our offscreen bitmap.
+            IntPtr handleDeviceContext = e.Graphics.GetHdc();
+            OpenGL.Blit(handleDeviceContext);
+            e.Graphics.ReleaseHdc(handleDeviceContext);
+
+            //	If's there's a GDI draw handler, then call it.
+            DoGDIDraw(new RenderEventArgs(e.Graphics));
+
+            //  Stop the stopwatch.
+            stopwatch.Stop();
+
+            //  Store the frame time.
+            frameTime = stopwatch.Elapsed.TotalMilliseconds;
+        }
+
+        public MyScene UIScene { get; set; }
     }
 }
