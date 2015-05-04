24be80dcea50621691d9351cc33b724d1535b256
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-04 14:41:02 +0800

    lower ColorIndicatorDataFactory's access

--- source/SharpGL/Core/SharpGL.SceneComponent/SimpleUI/ColorIndicator/ColorIndicatorDataFactory.cs
+++ source/SharpGL/Core/SharpGL.SceneComponent/SimpleUI/ColorIndicator/ColorIndicatorDataFactory.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 namespace SharpGL.SceneComponent
 {
-    public class ColorIndicatorDataFactory
+    class ColorIndicatorDataFactory
     {
         public static ColorIndicatorData CreateRainbow()
         {
