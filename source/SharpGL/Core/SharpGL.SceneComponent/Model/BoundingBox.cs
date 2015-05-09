ce32b196020618e9955fe8771061e63fe7bc9033
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-09 18:51:52 +0800

    clear code.

---
+++ source/SharpGL/Core/SharpGL.SceneComponent/Model/BoundingBox.cs
@@ -0,0 +1,77 @@
+﻿using SharpGL.Enumerations;
+using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpGL.SceneComponent
+{
+    public class BoundingBox
+    {
+        public Vertex MaxPosition { get; set; }
+        public Vertex MinPosition { get; set; }
+
+        public GLColor BoxColor { get; set; }
+
+        public BoundingBox()
+        {
+            BoxColor = new GLColor(1, 1, 1, 1);// white color
+        }
+
+        public Vertex GetCenter()
+        {
+            return (this.MaxPosition + this.MinPosition) / 2;
+        }
+
+        /// <summary>
+        /// Gets the bound dimensions.
+        /// </summary>
+        /// <param name="x">The x size.</param>
+        /// <param name="y">The y size.</param>
+        /// <param name="z">The z size.</param>
+        public void GetBoundDimensions(out float x, out float y, out float z)
+        {
+            Vertex diff = this.MaxPosition - this.MinPosition;
+            x = Math.Abs(diff.X);
+            y = Math.Abs(diff.Y);
+            z = Math.Abs(diff.Z);
+        }
+
+        /// <summary>
+        /// Render to the provided instance of OpenGL.
+        /// </summary>
+        /// <param name="gl">The OpenGL instance.</param>
+        /// <param name="renderMode">The render mode.</param>
+        public void Render(OpenGL gl, RenderMode renderMode)
+        {
+            gl.Color(BoxColor);
+
+            gl.Begin(BeginMode.LineLoop);
+            gl.Vertex(MinPosition.X, MinPosition.Y, MinPosition.Z);
+            gl.Vertex(MaxPosition.X, MinPosition.Y, MinPosition.Z);
+            gl.Vertex(MaxPosition.X, MinPosition.Y, MaxPosition.Z);
+            gl.Vertex(MinPosition.X, MinPosition.Y, MaxPosition.Z);
+            gl.End();
+
+            gl.Begin(BeginMode.LineLoop);
+            gl.Vertex(MinPosition.X, MaxPosition.Y, MinPosition.Z);
+            gl.Vertex(MaxPosition.X, MaxPosition.Y, MinPosition.Z);
+            gl.Vertex(MaxPosition.X, MaxPosition.Y, MaxPosition.Z);
+            gl.Vertex(MinPosition.X, MaxPosition.Y, MaxPosition.Z);
+            gl.End();
+
+            gl.Begin(BeginMode.Lines);
+            gl.Vertex(MinPosition.X, MinPosition.Y, MinPosition.Z);
+            gl.Vertex(MinPosition.X, MaxPosition.Y, MinPosition.Z);
+            gl.Vertex(MaxPosition.X, MinPosition.Y, MinPosition.Z);
+            gl.Vertex(MaxPosition.X, MaxPosition.Y, MinPosition.Z);
+            gl.Vertex(MaxPosition.X, MinPosition.Y, MaxPosition.Z);
+            gl.Vertex(MaxPosition.X, MaxPosition.Y, MaxPosition.Z);
+            gl.Vertex(MinPosition.X, MinPosition.Y, MaxPosition.Z);
+            gl.Vertex(MinPosition.X, MaxPosition.Y, MaxPosition.Z);
+            gl.End();
+        }
+    }
+}
---
+++ source/SharpGL/Core/SharpGL.SceneComponent/Model/ScientificModelElement.cs
@@ -0,0 +1,64 @@
+﻿using SharpGL.SceneGraph;
+using SharpGL.SceneGraph.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpGL.SceneComponent
+{
+    internal class ScientificModelElement : SceneElement, IRenderable, IHasObjectSpace
+    {
+        /// <summary>
+        /// All <see cref="ScientificModelElement"/>s use one same instance so that arcball calculation only happens once when user moves mouse.
+        /// </summary>
+        internal IMouseTransform modelTransform;
+
+        /// <summary>
+        /// The model shown in <see cref="ScientificVisual3DControl"/>.
+        /// </summary>
+        public IScientificModel Model { get; set; }
+
+        #region IRenderable 成员
+
+        public void Render(OpenGL gl, RenderMode renderMode)
+        {
+            IScientificModel model = this.Model;
+            if (model == null) { return; }
+
+            model.Render(gl, renderMode);
+            model.BoundingBox.Render(gl, renderMode);
+        }
+
+        #endregion
+
+        #region IHasObjectSpace 成员
+
+        public virtual void PushObjectSpace(OpenGL gl)
+        {
+            gl.PushMatrix();
+            if (this.Model == null) { return; }
+
+            gl.LoadIdentity();
+            Vertex center = this.Model.BoundingBox.GetCenter();
+            gl.Translate(center.X, center.Y, center.Z);//lastly, move model back.
+            modelTransform.TransformMatrix(gl);// then, rotate and scale model.
+            gl.Translate(-center.X, -center.Y, -center.Z);// firstly, move model to (0, 0, 0);
+            //gl.Translate(-center.X, -center.Y, -center.Z);//lastly, move model back.
+            //modelTransform.TransformMatrix(gl);// then, rotate and scale model.
+            //gl.Translate(center.X, center.Y, center.Z);// firstly, move model to (0, 0, 0);
+        }
+
+        public virtual void PopObjectSpace(OpenGL gl)
+        {
+            gl.PopMatrix();
+        }
+
+        public SceneGraph.Transformations.LinearTransformation Transformation
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+}
