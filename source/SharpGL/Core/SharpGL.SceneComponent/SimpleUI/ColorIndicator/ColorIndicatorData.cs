4c73214b9e9ab0056f359ba84200c0b06a5c4bbb
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-09 12:34:05 +0800

    add xml note. intergrate MapToColor() to ColorIndicatorData.

--- source/SharpGL/Core/SharpGL.SceneComponent/SimpleUI/ColorIndicator/ColorIndicatorData.cs
+++ source/SharpGL/Core/SharpGL.SceneComponent/SimpleUI/ColorIndicator/ColorIndicatorData.cs
@@ -2,13 +2,14 @@
 using SharpGL.SceneGraph;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
 namespace SharpGL.SceneComponent
 {
     /// <summary>
-    /// Contains color palette, block count, min value, max value etc.
+    /// Contains color palette, step, min value, max value etc.
     /// <para>Works as model while <see cref="SimpleUIColorIndicator"/> works as view.</para>
     /// </summary>
     public class ColorIndicatorData
@@ -47,7 +48,9 @@ namespace SharpGL.SceneComponent
         }
 
         private float step = 10.0f;
-
+        /// <summary>
+        /// Get or set distance for one block of color indicator's rectangle bar.
+        /// </summary>
         public float Step
         {
             get { return step; }
@@ -113,14 +116,26 @@ namespace SharpGL.SceneComponent
             }
         }
 
+        /// <summary>
+        /// Get mapping color of specified value.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public GLColor MapToColor(float value)
         {
-            return this.colorPalette.MapToColor(value, this.MinValue, this.MaxValue);
+            //return this.colorPalette.MapToColor(value, this.MinValue, this.MaxValue);
+            return this.MapToColor(value, this.colorPalette.Colors, this.colorPalette.Coords, this.minValue, this.maxValue);
         }
 
+        /// <summary>
+        /// Get block count at current maxVluae, minValue and step.
+        /// <para>Return -1 if step == 0</para>
+        /// <para>Return 1 if maxValue == minValue</para>
+        /// </summary>
+        /// <returns></returns>
         public int GetBlockCount()
         {
-            if (this.step <= 0) { return -1; }
+            if (this.step == 0) { return -1; }
 
             int blockCount = (int)Math.Ceiling((this.MaxValue - this.MinValue) / this.Step);
             if (blockCount == 0) { blockCount = 1; }
@@ -128,70 +143,56 @@ namespace SharpGL.SceneComponent
             return blockCount;
         }
 
-        //private GLColor[] colors;
+        GLColor MapToColor(float value, GLColor[] colors, float[] coords, float minValue, float maxValue)
+        {
+            if (colors == null || colors.Length < 2)
+            { throw new ArgumentNullException("colors", "colors' count must greater than 1."); }
+            if (coords == null || coords.Length != colors.Length)
+            { throw new ArgumentException("coords must have same count of elements with colors."); }
 
-        //public GLColor[] Colors
-        //{
-        //    get { return colors; }
-        //    set
-        //    {
-        //        if (value == null || value.Length < 2)
-        //        { throw new ArgumentNullException("colors", "colors' count must greater than 1."); }
-        //        colors = value;
-        //    }
-        //}
+            float difference = maxValue - minValue;
+            if (difference < 0.0f)
+            { throw new ArgumentException("fault value range"); }
 
-        //public ColorIndicatorData(GLColor[] colors, int minValue = 0, int maxValue = 0)
-        //{
-        //    if (colors == null || colors.Length < 2)
-        //    { throw new ArgumentNullException("colors", "colors' count must greater than 1."); }
-        //    if (maxValue < minValue)
-        //    { throw new Exception("minValue must less than or equal to maxValue."); }
+            if (difference == 0f)
+            { return colors[0]; }
 
-        //    this.Colors = colors;
-        //    this.minValue = minValue;
-        //    this.maxValue = maxValue;
-        //}
+            if (value <= minValue) { return colors[0]; }
+            if (value >= maxValue) { return colors[colors.Length - 1]; }
 
-        //public GLColor MapToColor(float value)
-        //{
-        //    GLColor result = MapToColor(value, this.minValue, this.maxValue);
-        //    return result;
-        //}
+            float progress = (value - minValue) / difference;
+            int leftIndex = -1;
+            for (int i = 1; i < coords.Length; i++)
+            {
+                if (progress < coords[i])
+                {
+                    leftIndex = i - 1;
+                    break;
+                }
+            }
 
-        //public GLColor MapToColor(float value, float minValue, float maxValue)
-        //{
-        //    GLColor[] colors = this.Colors;
-        //    if (colors == null || colors.Length < 2)
-        //    { throw new ArgumentNullException("colors", "colors' count must greater than 1."); }
+            if (leftIndex == -1)
+            {
+                Debug.WriteLine("MapToColor: progress[{0}] not found between minValue[{1}] and maxValue[{2}]! Coords (should be in [0, 1]) are: [{3}]",
+                    progress, minValue, maxValue,
+                    ArrayHelper.PrintArray(coords));
+                return colors[colors.Length - 1];
+            }
 
-        //    float difference = maxValue - minValue;
-        //    if (difference < 0.0f)
-        //    { throw new ArgumentException("fault value range"); }
+            float leftValue = minValue + difference * coords[leftIndex];
+            float rightValue = minValue + difference * coords[leftIndex + 1];
+            float blockWidth = rightValue - leftValue;
 
-        //    if (difference == 0f)
-        //    { return colors[0]; }
+            GLColor leftColor = colors[leftIndex];
+            GLColor rightColor = colors[leftIndex + 1];
 
-        //    if (value <= minValue) { return colors[0]; }
-        //    if (value >= maxValue) { return colors[colors.Length - 1]; }
+            GLColor color = leftColor * ((value - leftValue) / blockWidth)
+                + rightColor * ((rightValue - value) / blockWidth);
 
-        //    float step = difference / (colors.Length - 1);
+            return color;
+        }
 
-        //    int leftIndex = (int)Math.Floor(value - minValue / step);
-
-        //    float leftValue = leftIndex * step;
-        //    float rightValue = leftValue + step;
-
-        //    GLColor leftColor = colors[leftIndex];
-        //    GLColor rightColor = colors[leftIndex + 1];
-
-        //    GLColor color = new GLColor();
-        //    color = leftColor * ((value - leftValue) / step)
-        //        + rightColor * ((rightValue - value) / step);
-
-        //    return color;
-        //}
-
+     
         public override string ToString()
         {
             return string.Format("{0},min:{1}, max:{2}, step:{3}", colorPalette, minValue, maxValue, step);
---
+++ source/SharpGL/Core/SharpGL.SceneComponent/Utility/ArrayHelper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace System
+{
+    public static class ArrayHelper
+    {
+        /// <summary>
+        /// Print array's elements in format 'element, element, element, ...'
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static string PrintArray(this Array array)
+        {
+            if (array == null) { return string.Empty; }
+
+            StringBuilder builder = new StringBuilder();
+            foreach (var item in array)
+            {
+                builder.Append(item);
+                builder.Append(", ");
+            }
+
+            return builder.ToString();
+        }
+
+    }
+}
