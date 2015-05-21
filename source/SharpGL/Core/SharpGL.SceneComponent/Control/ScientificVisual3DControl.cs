305615c976ddce44cd7f3899a0848509828c394f
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-22 00:25:48 +0800

    add xml note to ScientificVisual3

--- source/SharpGL/Core/SharpGL.SceneComponent/Control/ScientificVisual3DControl.cs
+++ source/SharpGL/Core/SharpGL.SceneComponent/Control/ScientificVisual3DControl.cs
@@ -18,6 +18,7 @@ namespace SharpGL.SceneComponent
     /// <summary>
     /// Scene control which contains axis, color indicator, etc.
     /// <para>Set the <see cref="ScientificModel"/> property to view a model.</para>
+    /// <para>这个控件在Ortho模式的camera下，Line会被大量的点覆盖。这个问题暂时不知道是如何产生的。</para>
     /// </summary>
     public partial class ScientificVisual3DControl : MySceneControl
     {
