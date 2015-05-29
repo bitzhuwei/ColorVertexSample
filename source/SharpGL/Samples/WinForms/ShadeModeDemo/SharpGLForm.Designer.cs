ccd668619cfbe4f4da61ef525cd34930f15bd517
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-29 12:25:39 +0800

    add ShadeModeDemo project to show how glShadeMode() works.
    This is for color-coded picking when rendering triangle_strip,
    quad_strip, etc.

---
+++ source/SharpGL/Samples/WinForms/ShadeModeDemo/SharpGLForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ShadeModeDemo
+{
+    partial class SharpGLForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openGLControl = new SharpGL.OpenGLControl();
+            this.cmbBeginMode = new System.Windows.Forms.ComboBox();
+            this.cmbShadeMode = new System.Windows.Forms.ComboBox();
+            this.cmbRotation = new System.Windows.Forms.ComboBox();
+            this.txtModelInfo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // openGLControl
+            // 
+            this.openGLControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.openGLControl.DrawFPS = true;
+            this.openGLControl.Location = new System.Drawing.Point(181, 0);
+            this.openGLControl.Name = "openGLControl";
+            this.openGLControl.OpenGLVersion = SharpGL.Version.OpenGLVersion.OpenGL2_1;
+            this.openGLControl.RenderContextType = SharpGL.RenderContextType.FBO;
+            this.openGLControl.RenderTrigger = SharpGL.RenderTrigger.TimerBased;
+            this.openGLControl.Size = new System.Drawing.Size(443, 414);
+            this.openGLControl.TabIndex = 0;
+            this.openGLControl.OpenGLInitialized += new System.EventHandler(this.openGLControl_OpenGLInitialized);
+            this.openGLControl.OpenGLDraw += new SharpGL.RenderEventHandler(this.openGLControl_OpenGLDraw);
+            this.openGLControl.Resized += new System.EventHandler(this.openGLControl_Resized);
+            // 
+            // cmbBeginMode
+            // 
+            this.cmbBeginMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBeginMode.FormattingEnabled = true;
+            this.cmbBeginMode.Location = new System.Drawing.Point(12, 12);
+            this.cmbBeginMode.Name = "cmbBeginMode";
+            this.cmbBeginMode.Size = new System.Drawing.Size(163, 20);
+            this.cmbBeginMode.TabIndex = 1;
+            this.cmbBeginMode.SelectedIndexChanged += new System.EventHandler(this.cmbBeginMode_SelectedIndexChanged);
+            // 
+            // cmbShadeMode
+            // 
+            this.cmbShadeMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbShadeMode.FormattingEnabled = true;
+            this.cmbShadeMode.Location = new System.Drawing.Point(12, 38);
+            this.cmbShadeMode.Name = "cmbShadeMode";
+            this.cmbShadeMode.Size = new System.Drawing.Size(163, 20);
+            this.cmbShadeMode.TabIndex = 1;
+            this.cmbShadeMode.SelectedIndexChanged += new System.EventHandler(this.cmbShadeMode_SelectedIndexChanged);
+            // 
+            // cmbRotation
+            // 
+            this.cmbRotation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbRotation.FormattingEnabled = true;
+            this.cmbRotation.Location = new System.Drawing.Point(12, 64);
+            this.cmbRotation.Name = "cmbRotation";
+            this.cmbRotation.Size = new System.Drawing.Size(163, 20);
+            this.cmbRotation.TabIndex = 1;
+            this.cmbRotation.SelectedIndexChanged += new System.EventHandler(this.cmbRotation_SelectedIndexChanged);
+            // 
+            // txtModelInfo
+            // 
+            this.txtModelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtModelInfo.Location = new System.Drawing.Point(12, 90);
+            this.txtModelInfo.Multiline = true;
+            this.txtModelInfo.Name = "txtModelInfo";
+            this.txtModelInfo.ReadOnly = true;
+            this.txtModelInfo.Size = new System.Drawing.Size(163, 312);
+            this.txtModelInfo.TabIndex = 2;
+            // 
+            // SharpGLForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 414);
+            this.Controls.Add(this.txtModelInfo);
+            this.Controls.Add(this.cmbRotation);
+            this.Controls.Add(this.cmbShadeMode);
+            this.Controls.Add(this.cmbBeginMode);
+            this.Controls.Add(this.openGLControl);
+            this.Name = "SharpGLForm";
+            this.Text = "Shade Mode demo.";
+            this.Load += new System.EventHandler(this.SharpGLForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.openGLControl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private SharpGL.OpenGLControl openGLControl;
+        private System.Windows.Forms.ComboBox cmbBeginMode;
+        private System.Windows.Forms.ComboBox cmbShadeMode;
+        private System.Windows.Forms.ComboBox cmbRotation;
+        private System.Windows.Forms.TextBox txtModelInfo;
+    }
+}
+
---
+++ source/SharpGL/Samples/WinForms/ShadeModeDemo/SharpGLForm.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using SharpGL;
+using SharpGL.Enumerations;
+
+namespace ShadeModeDemo
+{
+    /// <summary>
+    /// The main form class.
+    /// </summary>
+    public partial class SharpGLForm : Form
+    {
+        private BeginMode beginMode;
+        private ShadeModel shadeMode;
+        private bool rotating;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SharpGLForm"/> class.
+        /// </summary>
+        public SharpGLForm()
+        {
+            InitializeComponent();
+
+            foreach (var name in Enum.GetNames(typeof(BeginMode)))
+            {
+                var item = new ComboBoxItem<BeginMode>(
+                    name, (BeginMode)Enum.Parse(typeof(BeginMode), name));
+                this.cmbBeginMode.Items.Add(item);
+            }
+            this.cmbBeginMode.SelectedIndex = 4;
+
+            foreach (var name in Enum.GetNames(typeof(ShadeModel)))
+            {
+                var item = new ComboBoxItem<ShadeModel>(
+                    name, (ShadeModel)Enum.Parse(typeof(ShadeModel), name));
+                this.cmbShadeMode.Items.Add(item);
+            }
+            this.cmbShadeMode.SelectedIndex = 1;
+
+            {
+                var rotating = new ComboBoxItem<bool>("Rotate", true);
+                this.cmbRotation.Items.Add(rotating);
+
+                var noRotating = new ComboBoxItem<bool>("No Rotate", false);
+                this.cmbRotation.Items.Add(noRotating);
+            }
+            this.cmbRotation.SelectedIndex = 0;
+        }
+
+        /// <summary>
+        /// Handles the OpenGLDraw event of the openGLControl control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="RenderEventArgs"/> instance containing the event data.</param>
+        private void openGLControl_OpenGLDraw(object sender, RenderEventArgs e)
+        {
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            gl.ClearColor(0.4f, 0.6f, 0.9f, 0.5f);
+
+            //  Clear the color and depth buffer.
+            gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
+
+            //  Load the identity matrix.
+            gl.LoadIdentity();
+
+            //  Rotate around the Y axis.
+            gl.Rotate(rotation, 0.0f, 1.0f, 0.0f);
+
+            gl.ShadeModel(this.shadeMode);
+
+            gl.Begin(this.beginMode);
+
+            //DrawPyramid(gl);
+            DrawModel(gl);
+
+            gl.End();
+
+            if (this.rotating)
+            {
+                //  Nudge the rotation.
+                rotation += 3.0f;
+            }
+        }
+
+        private void DrawModel(OpenGL gl)
+        {
+            //gl.Color(0.5f, 0.5f, 0.5f);
+            gl.Color(0, 0, 0);
+            gl.Vertex(0, 0);
+
+            gl.Color(1f, 0, 0);
+            gl.Vertex(0, 1);
+
+            gl.Color(0, 1f, 0);
+            gl.Vertex(1, 0);
+
+            gl.Color(1f, 1f, 0);
+            gl.Vertex(1, 1);
+
+            gl.Color(0, 0, 1f);
+            gl.Vertex(2, 0);
+
+            gl.Color(1f, 0, 1f);
+            gl.Vertex(2, 1);
+
+            gl.Color(0, 1f, 1f);
+            gl.Vertex(3, 0);
+
+            gl.Color(1f, 1f, 1f);
+            gl.Vertex(3, 1);
+        }
+
+        private static void DrawPyramid(OpenGL gl)
+        {
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(1.0f, -1.0f, 1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Color(1.0f, 0.0f, 0.0f);
+            gl.Vertex(0.0f, 1.0f, 0.0f);
+            gl.Color(0.0f, 0.0f, 1.0f);
+            gl.Vertex(-1.0f, -1.0f, -1.0f);
+            gl.Color(0.0f, 1.0f, 0.0f);
+            gl.Vertex(-1.0f, -1.0f, 1.0f);
+        }
+
+
+
+        /// <summary>
+        /// Handles the OpenGLInitialized event of the openGLControl control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void openGLControl_OpenGLInitialized(object sender, EventArgs e)
+        {
+            //  TODO: Initialise OpenGL here.
+
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Set the clear color.
+            gl.ClearColor(0, 0, 0, 0);
+        }
+
+        /// <summary>
+        /// Handles the Resized event of the openGLControl control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.EventArgs"/> instance containing the event data.</param>
+        private void openGLControl_Resized(object sender, EventArgs e)
+        {
+            //  TODO: Set the projection matrix here.
+
+            //  Get the OpenGL object.
+            OpenGL gl = openGLControl.OpenGL;
+
+            //  Set the projection matrix.
+            gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+            //  Load the identity.
+            gl.LoadIdentity();
+
+            //  Create a perspective transformation.
+            gl.Perspective(60.0f, (double)Width / (double)Height, 0.01, 100.0);
+
+            //  Use the 'look at' helper function to position and aim the camera.
+            //gl.LookAt(-5, 5, -5, 0, 0, 0, 0, 1, 0);
+            gl.LookAt(-3, 3, -3, 0, 0, 0, 0, 1, 0);
+
+            //  Set the modelview matrix.
+            gl.MatrixMode(OpenGL.GL_MODELVIEW);
+        }
+
+        /// <summary>
+        /// The current rotation.
+        /// </summary>
+        private float rotation = 0.0f;
+
+        private void SharpGLForm_Load(object sender, EventArgs e)
+        {
+            this.txtModelInfo.Text = @"gl.Color(0, 0, 0);
+gl.Vertex(0, 0);
+
+gl.Color(1f, 0, 0);
+gl.Vertex(0, 1);
+
+gl.Color(0, 1f, 0);
+gl.Vertex(1, 0);
+
+gl.Color(1f, 1f, 0);
+gl.Vertex(1, 1);
+
+gl.Color(0, 0, 1f);
+gl.Vertex(2, 0);
+
+gl.Color(1f, 0, 1f);
+gl.Vertex(2, 1);
+
+gl.Color(0, 1f, 1f);
+gl.Vertex(3, 0);
+
+gl.Color(1f, 1f, 1f);
+gl.Vertex(3, 1);";
+        }
+
+        private void cmbBeginMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = cmbBeginMode.SelectedItem as ComboBoxItem<BeginMode>;
+            this.beginMode = item.value;
+        }
+
+        private void cmbShadeMode_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = cmbShadeMode.SelectedItem as ComboBoxItem<ShadeModel>;
+            this.shadeMode = item.value;
+        }
+
+        private void cmbRotation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var item = cmbRotation.SelectedItem as ComboBoxItem<bool>;
+            this.rotating = item.value;
+        }
+    }
+
+    class ComboBoxItem<T>
+    {
+        public T value;
+        public string name;
+
+        public ComboBoxItem(string name, T value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}", name);
+            //return base.ToString();
+        }
+    }
+}
