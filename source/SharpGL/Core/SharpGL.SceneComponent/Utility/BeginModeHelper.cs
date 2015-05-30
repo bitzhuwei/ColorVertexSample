01ac166e9a6074702112ffb65c350f893ec1137d
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-30 11:42:01 +0800

    add BeginModeHelper.GetPrimitiveCount()

--- source/SharpGL/Core/SharpGL.SceneComponent/Utility/BeginModeHelper.cs
+++ source/SharpGL/Core/SharpGL.SceneComponent/Utility/BeginModeHelper.cs
@@ -54,5 +54,56 @@ namespace SharpGL.SceneComponent
 
             return result;
         }
+
+        /// <summary>
+        /// Get primitive's count according to specified <paramref name="mode"/> and <paramref name="vertexCount"/>.
+        /// </summary>
+        /// <param name="mode"></param>
+        /// <param name="vertexCount"></param>
+        /// <returns></returns>
+        public static int GetPrimitiveCount(SharpGL.Enumerations.BeginMode mode, int vertexCount)
+        {
+            if (vertexCount <= 0) { return 0; }
+
+            int result = 0;
+
+            switch (mode)
+            {
+                case SharpGL.Enumerations.BeginMode.Points:
+                    result = vertexCount;
+                    break;
+                case SharpGL.Enumerations.BeginMode.Lines:
+                    result = vertexCount / 2;
+                    break;
+                case SharpGL.Enumerations.BeginMode.LineLoop:
+                    result = vertexCount;
+                    break;
+                case SharpGL.Enumerations.BeginMode.LineStrip:
+                    result = vertexCount - 1;
+                    break;
+                case SharpGL.Enumerations.BeginMode.Triangles:
+                    result = vertexCount / 3;
+                    break;
+                case SharpGL.Enumerations.BeginMode.TriangleString:
+                    result = vertexCount - 2;
+                    break;
+                case SharpGL.Enumerations.BeginMode.TriangleFan:
+                    result = vertexCount - 2;
+                    break;
+                case SharpGL.Enumerations.BeginMode.Quads:
+                    result = vertexCount / 4;
+                    break;
+                case SharpGL.Enumerations.BeginMode.QuadStrip:
+                    result = vertexCount / 2 - 1;
+                    break;
+                case SharpGL.Enumerations.BeginMode.Polygon:
+                    result = 1;
+                    break;
+                default:
+                    throw new NotImplementedException();
+            }
+
+            return result;
+        }
     }
 }
