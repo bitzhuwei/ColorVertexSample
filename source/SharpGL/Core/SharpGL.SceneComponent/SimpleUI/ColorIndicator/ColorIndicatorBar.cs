b96b42b714866d2442fe7a387e9e487361f85907
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-31 01:13:52 +0800

    ColorIndicatorBar's settings are OK again.

--- source/SharpGL/Core/SharpGL.SceneComponent/SimpleUI/ColorIndicator/ColorIndicatorBar.cs
+++ source/SharpGL/Core/SharpGL.SceneComponent/SimpleUI/ColorIndicator/ColorIndicatorBar.cs
@@ -50,45 +50,45 @@ namespace SharpGL.SceneComponent
         {
             int blockCount = data.GetBlockCount();
             //int blockCount = data.BlockCount;
-            int length = blockCount + 1;
-            ScientificModel verticalLines = new ScientificModel(length * 2, Enumerations.BeginMode.Lines);
+            int segmentCount = blockCount + 1;
+            ScientificModel verticalLines = new ScientificModel(segmentCount * 2, Enumerations.BeginMode.Lines);
             float[] positions = verticalLines.Positions;
-            for (int i = 0; i < length * 3; i += 3)
+            for (int i = 0; i < segmentCount; i++)
             {
-                if (i + 1 != length)
+                if (i + 1 != segmentCount)
                 {
                     if (data.MaxValue != data.MinValue)
                     {
-                        positions[i * 2 + 0] = barWidth * (i * data.Step / (data.MaxValue - data.MinValue));
+                        positions[i * 2 * 3 + 0] = barWidth * (i * data.Step / (data.MaxValue - data.MinValue));
                     }
                     else
                     {
-                        positions[i * 2 + 0] = barWidth * 0;
+                        positions[i * 2 * 3 + 0] = barWidth * 0;
                     }
                 }
                 else
                 {
-                    positions[i * 2 + 0] = barWidth;
+                    positions[i * 2 * 3 + 0] = barWidth;
                 }
-                positions[i * 2 + 1] = -9;
-                positions[i * 2 + 2] = 0;
-                positions[i * 2 + 1 + 0] = positions[i * 2 + 0];
-                positions[i * 2 + 1 + 1] = barHeight;
-                positions[i * 2 + 1 + 2] = 0;
+                positions[i * 2 * 3 + 1] = -9;
+                positions[i * 2 * 3 + 2] = 0;
+                positions[i * 2 * 3 + 3 + 0] = positions[i * 2 * 3 + 0];
+                positions[i * 2 * 3 + 3 + 1] = barHeight;
+                positions[i * 2 * 3 + 3 + 2] = 0;
             }
             // move the vertical lines' center to (0, 0, 0)
-            for (int i = 0; i < length * 2 * 3; i += 3)
+            for (int i = 0; i < segmentCount * 2; i++)
             {
-                positions[i + 0] -= barWidth / 2;
-                positions[i + 1] -= barHeight / 2;
+                positions[i * 3 + 0] -= barWidth / 2;
+                positions[i * 3 + 1] -= barHeight / 2;
             }
 
             float[] colors = verticalLines.Colors;
-            for (int i = 0; i < length * 2 * 3; i += 3)
+            for (int i = 0; i < segmentCount * 2; i++)
             {
-                colors[i + 0] = byte.MaxValue / 2;
-                colors[i + 1] = byte.MaxValue / 2;
-                colors[i + 2] = byte.MaxValue / 2;
+                colors[i * 3 + 0] = 1;
+                colors[i * 3 + 1] = 1;
+                colors[i * 3 + 2] = 1;
                 //colors[i].red = byte.MaxValue / 2;
                 //colors[i].green = byte.MaxValue / 2;
                 //colors[i].blue = byte.MaxValue / 2;
@@ -108,24 +108,24 @@ namespace SharpGL.SceneComponent
             //positions[3].X = barWidth;
             //positions[3].Y = barHeight;
             //positions[3].Z = 0;
-            positions[0 + 0] = 0; positions[0 + 1] = 0; positions[0 + 2] = 0;
-            positions[1 + 0] = barWidth; positions[1 + 1] = 0; positions[1 + 2] = 0;
-            positions[2 + 0] = 0; positions[2 + 1] = barHeight; positions[2 + 2] = 0;
-            positions[3 + 0] = barWidth;
-            positions[3 + 1] = barHeight;
-            positions[3 + 2] = 0;
+            positions[0 * 3 + 0] = 0; positions[0 * 3 + 1] = 0; positions[0 * 3 + 2] = 0;
+            positions[1 * 3 + 0] = barWidth; positions[1 * 3 + 1] = 0; positions[1 * 3 + 2] = 0;
+            positions[2 * 3 + 0] = 0; positions[2 * 3 + 1] = barHeight; positions[2 * 3 + 2] = 0;
+            positions[3 * 3 + 0] = barWidth;
+            positions[3 * 3 + 1] = barHeight;
+            positions[3 * 3 + 2] = 0;
             // move the horizontal white lines' center to (0, 0, 0)
-            for (int i = 0; i < length * 3; i += 3)
+            for (int i = 0; i < length; i++)
             {
-                positions[i + 0] -= barWidth / 2;
-                positions[i + 1] -= barHeight / 2;
+                positions[i * 3 + 0] -= barWidth / 2;
+                positions[i * 3 + 1] -= barHeight / 2;
             }
             float[] colors = horizontalLines.Colors;
-            for (int i = 0; i < length * 3; i += 3)
+            for (int i = 0; i < length; i++)
             {
-                colors[i + 0] = byte.MaxValue / 2;
-                colors[i + 1] = byte.MaxValue / 2;
-                colors[i + 2] = byte.MaxValue / 2;
+                colors[i * 3 + 0] = 1;
+                colors[i * 3 + 1] = 1;
+                colors[i * 3 + 2] = 1;
                 //colors[i].red = byte.MaxValue / 2;
                 //colors[i].green = byte.MaxValue / 2;
                 //colors[i].blue = byte.MaxValue / 2;
@@ -136,35 +136,35 @@ namespace SharpGL.SceneComponent
 
         unsafe private void GenerateRectangles(ColorIndicatorData data)
         {
-            int length = data.ColorPalette.Colors.Length;
-            ScientificModel rectModel = new ScientificModel(length * 2, Enumerations.BeginMode.QuadStrip);
+            int rectCount = data.ColorPalette.Colors.Length;
+            ScientificModel rectModel = new ScientificModel(rectCount * 2, Enumerations.BeginMode.QuadStrip);
             float[] positions = rectModel.Positions;
-            for (int i = 0; i < length * 3; i += 3)
+            for (int i = 0; i < rectCount; i++)
             {
-                positions[i * 2 + 0] = barWidth * data.ColorPalette.Coords[i / 3];
-                positions[i * 2 + 1] = 0;
-                positions[i * 2 + 2] = 0;
-                positions[i * 2 + 1 + 0] = positions[i * 2];
-                positions[i * 2 + 1 + 1] = barHeight;
-                positions[i * 2 + 1 + 2] = 0;
+                positions[i * 2 * 3 + 0] = barWidth * data.ColorPalette.Coords[i];
+                positions[i * 2 * 3 + 1] = 0;
+                positions[i * 2 * 3 + 2] = 0;
+                positions[i * 2 * 3 + 3 + 0] = positions[i * 2 * 3];
+                positions[i * 2 * 3 + 3 + 1] = barHeight;
+                positions[i * 2 * 3 + 3 + 2] = 0;
             }
             // move the rectangles' center to (0, 0, 0)
-            for (int i = 0; i < length * 2 * 3; i += 3)
+            for (int i = 0; i < rectCount * 2; i++)
             {
-                positions[i + 0] -= barWidth / 2;
-                positions[i + 1] -= barHeight / 2;
+                positions[i * 3 + 0] -= barWidth / 2;
+                positions[i * 3 + 1] -= barHeight / 2;
             }
 
             float[] colors = rectModel.Colors;
-            for (int i = 0; i < length * 3; i += 3)
+            for (int i = 0; i < rectCount; i++)
             {
-                GLColor color = data.ColorPalette.Colors[i / 3];
-                colors[i * 2 + 0] = (byte)(color.R * byte.MaxValue / 2);
-                colors[i * 2 + 1] = (byte)(color.G * byte.MaxValue / 2);
-                colors[i * 2 + 2] = (byte)(color.B * byte.MaxValue / 2);
-                colors[i * 2 + 1 + 0] = (byte)(color.R * byte.MaxValue / 2);
-                colors[i * 2 + 1 + 1] = (byte)(color.G * byte.MaxValue / 2);
-                colors[i * 2 + 1 + 2] = (byte)(color.B * byte.MaxValue / 2);
+                GLColor color = data.ColorPalette.Colors[i];
+                colors[i * 2 * 3 + 0] = (color.R);
+                colors[i * 2 * 3 + 1] = (color.G);
+                colors[i * 2 * 3 + 2] = (color.B);
+                colors[i * 2 * 3 + 3 + 0] = (color.R);
+                colors[i * 2 * 3 + 3 + 1] = (color.G);
+                colors[i * 2 * 3 + 3 + 2] = (color.B);
             }
 
             this.rectModel = rectModel;
