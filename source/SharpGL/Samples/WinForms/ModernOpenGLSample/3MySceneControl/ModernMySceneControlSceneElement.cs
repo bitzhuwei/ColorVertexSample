207ff1f965ce9917fddb220f0c962cf485c81adf
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-05-31 13:07:29 +0800

    remove repeated PickedPrimitive in demo project.

--- source/SharpGL/Samples/WinForms/ModernOpenGLSample/3MySceneControl/ModernMySceneControlSceneElement.cs
+++ source/SharpGL/Samples/WinForms/ModernOpenGLSample/3MySceneControl/ModernMySceneControlSceneElement.cs
@@ -355,37 +355,5 @@ namespace ModernOpenGLSample._3MySceneControl
         #endregion
     }
 
-    /// <summary>
-    /// The color-coded picking result.
-    /// <para>Representing a primitive.</para>
-    /// </summary>
-    public class PickedPrimitive : IPickedPrimitive
-    {
-        /// <summary>
-        /// Gets or sets primitive's type.
-        /// </summary>
-        public PrimitiveType Type { get; set; }
-
-        /// <summary>
-        /// Gets or sets values of this primitive.
-        /// </summary>
-        public float[] positions { get; set; }
-
-        /// <summary>
-        /// The element that this picked primitive belongs to.
-        /// </summary>
-        public IColorCodedPicking Element { get; set; }
-
-#if DEBUG
-        public int StageVertexID { get; set; }
-#endif
-
-        public override string ToString()
-        {
-            string result = string.Format("{0}:{1}|belong to:{2}", Type, positions.PrintVectors(), Element);
-            return result;
-            //return base.ToString();
-        }
-
-    }
+    
 }
