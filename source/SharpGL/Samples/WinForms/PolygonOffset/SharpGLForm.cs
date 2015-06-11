5668f9b0bfd35b8ed2246c0f6771436cc585c165
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-06-12 00:27:45 +0800

    enable satellite rotation.

--- source/SharpGL/Samples/WinForms/PolygonOffset/SharpGLForm.cs
+++ source/SharpGL/Samples/WinForms/PolygonOffset/SharpGLForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using SharpGL;
+using SharpGL.SceneComponent;
 
 namespace PolygonOffset
 {
@@ -15,12 +16,61 @@ namespace PolygonOffset
     /// </summary>
     public partial class SharpGLForm : Form
     {
+        SharpGL.SceneComponent.SatelliteRotation satelliteRotation = new SharpGL.SceneComponent.SatelliteRotation();
+        SharpGL.SceneComponent.ScientificCamera camera = new SharpGL.SceneComponent.ScientificCamera(SharpGL.SceneComponent.CameraTypes.Perspecitive);
+
         /// <summary>
         /// Initializes a new instance of the <see cref="SharpGLForm"/> class.
         /// </summary>
         public SharpGLForm()
         {
             InitializeComponent();
+
+            SharpGL.SceneComponent.ScientificCamera camera = this.camera;
+
+            this.satelliteRotation.Camera = camera;
+
+            camera.Position = new SharpGL.SceneGraph.Vertex(-5, 5, -5);
+            camera.Target = new SharpGL.SceneGraph.Vertex();
+            camera.UpVector = new SharpGL.SceneGraph.Vertex(0, 1, 0);
+
+            this.openGLControl_Resized(this.openGLControl, EventArgs.Empty);
+
+            this.openGLControl.MouseDown += openGLControl_MouseDown;
+            this.openGLControl.MouseMove += openGLControl_MouseMove;
+            this.openGLControl.MouseUp += openGLControl_MouseUp;
+        }
+
+        void openGLControl_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.satelliteRotation.MouseUp(e.X, e.Y);
+        }
+
+        void openGLControl_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.satelliteRotation.MouseMove(e.X, e.Y);
+            if (this.satelliteRotation.mouseDownFlag)
+            {
+                //  Get the OpenGL object.
+                OpenGL gl = openGLControl.OpenGL;
+
+                //  Set the projection matrix.
+                gl.MatrixMode(OpenGL.GL_PROJECTION);
+
+                //  Load the identity.
+                gl.LoadIdentity();
+
+                camera.TransformProjectionMatrix(gl);
+
+                //  Set the modelview matrix.
+                gl.MatrixMode(OpenGL.GL_MODELVIEW);
+            }
+        }
+
+        void openGLControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.satelliteRotation.SetBounds(this.openGLControl.Width, this.openGLControl.Height);
+            this.satelliteRotation.MouseDown(e.X, e.Y);
         }
 
         /// <summary>
@@ -101,6 +151,13 @@ namespace PolygonOffset
         {
             //  TODO: Set the projection matrix here.
 
+            SharpGL.SceneComponent.ScientificCamera camera = this.camera;
+            SharpGL.SceneComponent.IPerspectiveViewCamera perspective = camera;
+            perspective.AspectRatio = (double)Width / (double)Height;
+            perspective.Far = 100;
+            perspective.Near = 0.01;
+            perspective.FieldOfView = 60;
+
             //  Get the OpenGL object.
             OpenGL gl = openGLControl.OpenGL;
 
@@ -110,11 +167,7 @@ namespace PolygonOffset
             //  Load the identity.
             gl.LoadIdentity();
 
-            //  Create a perspective transformation.
-            gl.Perspective(60.0f, (double)Width / (double)Height, 0.01, 100.0);
-
-            //  Use the 'look at' helper function to position and aim the camera.
-            gl.LookAt(-5, 5, -5, 0, 0, 0, 0, 1, 0);
+            camera.TransformProjectionMatrix(gl);
 
             //  Set the modelview matrix.
             gl.MatrixMode(OpenGL.GL_MODELVIEW);
@@ -124,5 +177,6 @@ namespace PolygonOffset
         /// The current rotation.
         /// </summary>
         private float rotation = 0.0f;
+
     }
 }
