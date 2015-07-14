66e1eba2762d97f36f61dd94c8e44a607da993d4
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2015-07-15 01:00:25 +0800

    UnmanagedArray takes cares of bytes from Marshal.AllocHGlobal().

---
+++ source/SharpGL/Samples/WinForms/YieldingGeometryModel/UnmanagedArray.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YieldingGeometryModel
+{
+    /// <summary>
+    /// 用于记录非托管的数组。
+    /// </summary>
+    public class UnmanagedArray : IDisposable
+    {
+        /// <summary>
+        /// 用于记录非托管的数组。
+        /// </summary>
+        /// <param name="byteLength">所需的内存中的字节数。</param>
+        public UnmanagedArray(int byteLength)
+        {
+            this.pointer = Marshal.AllocHGlobal(byteLength);
+            this.byteLength = byteLength;
+        }
+        /// <summary>
+        /// 数组指针。
+        /// </summary>
+        public IntPtr pointer;
+
+        /// <summary>
+        /// 数组元素数 * 元素长度。
+        /// </summary>
+        public int byteLength;
+
+
+        #region IDisposable Members
+
+        /// <summary>
+        /// Internal variable which checks if Dispose has already been called
+        /// </summary>
+        private Boolean disposed;
+
+        /// <summary>
+        /// Releases unmanaged and - optionally - managed resources
+        /// </summary>
+        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+        private void Dispose(Boolean disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                //TODO: Managed cleanup code here, while managed refs still valid
+            }
+            //TODO: Unmanaged cleanup code here
+            Marshal.FreeHGlobal(this.pointer);
+
+            disposed = true;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            // Call the private Dispose(bool) helper and indicate 
+            // that we are explicitly disposing
+            this.Dispose(true);
+
+            // Tell the garbage collector that the object doesn't require any
+            // cleanup when collected since Dispose was called explicitly.
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+				
+    }
+}
