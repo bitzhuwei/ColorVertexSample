d64789552f755d8eb7d2dbfce4242f4beec1d3f7
祝威 <bitzhuwei.cnblogs@qq.com>
2015-10-17 00:55:31 +0800

    add iDisposable to element: stage 1

--- source/SharpGL/Samples/WinForms/YieldingGeometryModel/HexahedronGridderElement_IDisposable.cs
+++ source/SharpGL/Samples/WinForms/YieldingGeometryModel/HexahedronGridderElement_IDisposable.cs
@@ -16,8 +16,57 @@ using YieldingGeometryModel.GLPrimitive;
 
 namespace YieldingGeometryModel
 {
-    public partial class HexahedronGridderElement 
+    public partial class HexahedronGridderElement : IDisposable
     {
+
+        #region IDisposable Members
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        } // end sub
+
+        /// <summary>
+        /// Destruct instance of the class.
+        /// </summary>
+        ~HexahedronGridderElement()
+        {
+            this.Dispose(false);
+        }
+
+        /// <summary>
+        /// Backing field to track whether Dispose has been called.
+        /// </summary>
+        private bool disposedValue = false;
+
+        /// <summary>
+        /// Dispose managed and unmanaged resources of this instance.
+        /// </summary>
+        /// <param name="disposing">If disposing equals true, managed and unmanaged resources can be disposed. If disposing equals false, only unmanaged resources can be disposed. </param>
+        protected virtual void Dispose(bool disposing)
+        {
+
+            if (this.disposedValue == false)
+            {
+                if (disposing)
+                {
+                    // TODO: Dispose managed resources.
+                } // end if
+
+                // TODO: Dispose unmanaged resources.
+                OpenGL gl=new OpenGL();
+                    
+                this.colorsBufferObject
+            } // end if
+
+            this.disposedValue = true;
+        } // end sub
+
+        #endregion
 
         protected override void CleanUnmanagedRes()
         {
