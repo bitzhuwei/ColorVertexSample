0006ea308d659b495860f68cca8fcfda6688b3fd
祝威 <bitzhuwei.cnblogs@qq.com>
2015-12-03 10:54:52 +0800

    test case for unsafe method of using

---
+++ source/SharpGL/Samples/WinForms/UnmanagedArrayTest/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace UnmanagedArrayTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInSubRoutine = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnInSubRoutine
+            // 
+            this.btnInSubRoutine.Location = new System.Drawing.Point(12, 12);
+            this.btnInSubRoutine.Name = "btnInSubRoutine";
+            this.btnInSubRoutine.Size = new System.Drawing.Size(482, 33);
+            this.btnInSubRoutine.TabIndex = 0;
+            this.btnInSubRoutine.Text = "在子函数中不断使用UnmanagedArray";
+            this.btnInSubRoutine.UseVisualStyleBackColor = true;
+            this.btnInSubRoutine.Click += new System.EventHandler(this.btnInSubRoutine_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 291);
+            this.Controls.Add(this.btnInSubRoutine);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInSubRoutine;
+    }
+}
+
---
+++ source/SharpGL/Samples/WinForms/UnmanagedArrayTest/Form1.cs
@@ -0,0 +1,71 @@
+﻿using GlmNet;
+using SharpGL.SceneComponent;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnmanagedArrayTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInSubRoutine_Click(object sender, EventArgs e)
+        {
+            int count = 1000000;
+            // 测试vec3类型
+            {
+                var vec3Array = new UnmanagedArray<vec3>(count);
+                long startTick = DateTime.Now.Ticks;
+                for (int i = 0; i < count; i++)
+                {
+                    vec3Array[i] = new vec3(i * 3 + 0, i * 3 + 1, i * 3 + 2);
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    var item = vec3Array[i];
+                    var old = new vec3(i * 3 + 0, i * 3 + 1, i * 3 + 2);
+                    if (item.x != old.x || item.y != old.y || item.z != old.z)
+                    { throw new Exception(); }
+                }
+                long interval = DateTime.Now.Ticks - startTick;
+
+                unsafe
+                {
+                    startTick = DateTime.Now.Ticks;
+                    vec3* header = (vec3*)vec3Array.FirstElement();
+                    vec3* last = (vec3*)vec3Array.LastElement();
+                    vec3* tailAddress = (vec3*)vec3Array.TailAddress();
+                    int i = 0;
+                    for (vec3* ptr = header; ptr <= last/*or: ptr < tailAddress*/; ptr++)
+                    {
+                        *ptr = new vec3(i * 3 + 0, i * 3 + 1, i * 3 + 2);
+                        i++;
+                    }
+                    i = 0;
+                    for (vec3* ptr = header; ptr <= last/*or: ptr < tailAddress*/; ptr++, i++)
+                    {
+                        var item = *ptr;
+                        var old = new vec3(i * 3 + 0, i * 3 + 1, i * 3 + 2);
+                        if (item.x != old.x || item.y != old.y || item.z != old.z)
+                        { throw new Exception(); }
+                    }
+                }
+                long interval2 = DateTime.Now.Ticks - startTick;
+
+                Console.WriteLine();
+                MessageBox.Show(string.Format("Ticks: safe: {0} vs unsafe: {1}", interval, interval2), "result");
+
+            }
+        }
+    }
+}
