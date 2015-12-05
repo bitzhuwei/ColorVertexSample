1c3f5cffb0c448b133ba30495f0b232a0d5ed5ec
祝威 <bitzhuwei.cnblogs@qq.com>
2015-12-05 17:25:05 +0800

    Renderer works but maybe there's nowhere it can

--- source/SharpGL/Simlab/SimLabDesign1/Renderer.cs
+++ source/SharpGL/Simlab/SimLabDesign1/Renderer.cs
@@ -1,5 +1,8 @@
-﻿using SharpGL;
+﻿using GlmNet;
+using SharpGL;
+using SharpGL.SceneComponent;
 using SharpGL.SceneGraph.Core;
+using SharpGL.Shaders;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -28,11 +31,13 @@ namespace SimLabDesign1
     {
         /// <summary>
         /// count in DrawElements(uint mode, int count, uint type, IntPtr indices);
+        /// <para>要渲染多少个索引。</para>
         /// </summary>
         public int Count { get; set; }
 
         /// <summary>
         /// type in DrawElements(uint mode, int count, uint type, IntPtr indices);
+        /// <para>OpenGL.GL_UNSIGNED_INT等</para>
         /// </summary>
         public uint Type { get; set; }
 
@@ -49,23 +54,42 @@ namespace SimLabDesign1
     public class NonIndexRenderer : Renderer
     {
         /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="vertexCountPerPrim">VBO中每个单元（一个三角形、一个六面体等等）包含的顶点数。</param>
+        /// <param name="PrimCount">VBO中包含的单元（一个三角形、一个六面体）的数目。</param>
+        public NonIndexRenderer(int vertexCountPerPrim, int PrimCount)
+        {
+            this.First = new int[PrimCount];
+            this.Count = new int[PrimCount];
+            for (int i = 0; i < this.First.Length; i++)
+            {
+                this.First[i] = i * vertexCountPerPrim;
+                this.Count[i] = vertexCountPerPrim;
+            }
+            this.PrimCount = PrimCount;
+        }
+        /// <summary>
         /// first in MultiDrawArrays(uint mode, int[] first, int[] count, int primcount)
         /// </summary>
-        int[] First { get; set; }
+        public int[] First { get; set; }
 
         /// <summary>
         /// count in MultiDrawArrays(uint mode, int[] first, int[] count, int primcount)
         /// </summary>
-        int[] Count { get; set; }
+        public int[] Count { get; set; }
 
         /// <summary>
         /// primcount in MultiDrawArrays(uint mode, int[] first, int[] count, int primcount)
         /// </summary>
-        int PrimCount { get; set; }
+        public int PrimCount { get; set; }
 
         public override void Render(OpenGL gl, RenderMode renderMode)
         {
+
             gl.MultiDrawArrays(this.Mode, this.First, this.Count, this.PrimCount);
         }
+
+
     }
 }
