21630998b3835209cd0fad4058d2f0a911791f23
祝威 <bitzhuwei.cnblogs@qq.com>
2015-12-09 18:13:56 +0800

    fix

--- source/SharpGL/Core/SharpGL.SceneComponent/Control/ColorIndicatorTextureExtension.cs
+++ source/SharpGL/Core/SharpGL.SceneComponent/Control/ColorIndicatorTextureExtension.cs
@@ -1,4 +1,5 @@
 ﻿using SharpGL.SceneComponent.SimpleUI.ColorIndicator;
+using SharpGL.SceneGraph;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
@@ -13,11 +14,17 @@ namespace SharpGL.SceneComponent
     public static class ColorIndicatorTextureExtension
     {
 
-        public static Bitmap CreateTextureImage(this SimpleUIColorIndicator colorIndicator, int width = 1000)
-        {
-            return colorIndicator.Data.ColorPalette.CreateTextureImage(width, 20);
-        }
+        public static Bitmap CreateTextureImage(this SimpleUIColorIndicator colorIndicator, int width = 100000)
 
+        {
+            return colorIndicator.Data.ColorPalette.CreateTextureImage(width, 50);
+        }
+        class vec3
+        {
+            public float r;
+            public float g;
+            public float b;
+        }
         /// <summary>
         /// 根据色板获取位图。
         /// </summary>
@@ -26,22 +33,53 @@ namespace SharpGL.SceneComponent
         private static Bitmap CreateTextureImage(this ColorPalette colorPalette, int width = 1000, int height = 20)
         {
             Bitmap bitmap = new Bitmap(width, height);
-            Graphics g = Graphics.FromImage(bitmap);
+            //Graphics g = Graphics.FromImage(bitmap);
 
             for (int i = 0; i < colorPalette.Colors.Length - 1; i++)
             {
                 int left = (int)(width * colorPalette.Coords[i]);
                 int right = (int)(width * colorPalette.Coords[i + 1]);
-                //注意，最后一个色块的最后一个像素没有画到bitmap上。
-                Rectangle rect = new Rectangle(left, 0, right - left + 1, height);
-                LinearGradientBrush brush = new LinearGradientBrush(
-                    rect,
-                    colorPalette.Colors[i], colorPalette.Colors[i + 1], LinearGradientMode.Horizontal);
-                g.FillRectangle(brush, rect);
+                GLColor leftColor = colorPalette.Colors[i];
+                GLColor rightColor = colorPalette.Colors[i + 1];
+                for (int x = left; x < right; x++)
+                {
+                    Color color = (leftColor * ((right - x) * 1.0f / (right - left)) + rightColor * ((x - left) * 1.0f / (right - left)));
+                    for (int y = 0; y < height; y++)
+                    {
+                        bitmap.SetPixel(x, y, color);
+                    }
+                }
             }
-            g.Dispose();
+            //g.Dispose();
             return bitmap;
         }
+
+        ///// <summary>
+        ///// 根据色板获取位图。
+        ///// </summary>
+        ///// <param name="colorPalette"></param>
+        ///// <returns></returns>
+        //private static Bitmap CreateTextureImage(this ColorPalette colorPalette, int width = 1000, int height = 20)
+        //{
+        //    Bitmap bitmap = new Bitmap(width, height);
+        //    Graphics g = Graphics.FromImage(bitmap);
+
+        //    for (int i = 0; i < colorPalette.Colors.Length - 1; i++)
+        //    {
+        //        int left = (int)(width * colorPalette.Coords[i]);
+        //        int right = (int)(width * colorPalette.Coords[i + 1]);
+        //        //注意，最后一个色块的最后一个像素没有画到bitmap上。
+        //        if (right == width) { right = width - 1; }
+        //        Rectangle rect = new Rectangle(left, 0, right - left + 1, height);
+        //        LinearGradientBrush brush = new LinearGradientBrush(
+        //            rect,
+        //            colorPalette.Colors[i], colorPalette.Colors[i + 1], LinearGradientMode.Horizontal);
+        //        g.FillRectangle(brush, rect);
+        //    }
+        //    g.Dispose();
+        //    return bitmap;
+        //}
+
         ///// <summary>
         ///// 根据色板获取位图。
         ///// </summary>
