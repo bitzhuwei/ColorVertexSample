7cf51f18039a9c594816c15fd83df6f4ee284d65
dalanmao <14881236@qq.com>
2016-03-04 18:54:24 +0800

    missing files

---
+++ source/SharpGL/Simlab/SimLab/Geometry/QuadPosition.cs
@@ -0,0 +1,24 @@
+﻿using SharpGL.SceneGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimLab.Geometry
+{
+
+    /// <summary>
+    /// 四边形描述信息,p1,p2，p3,p4，理解为在一个平面上。四个边组成为
+    /// (p1,p2) (p3,p4) (p1,p3) (p2,p4) 四边组成一个平面
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct QuadPosition
+    {
+        public Vertex P1;
+        public Vertex P2;
+        public Vertex P3;
+        public Vertex P4;
+    }
+}
---
+++ source/SharpGL/Simlab/SimLab/Geometry/TrianglularPrismPosition.cs
@@ -0,0 +1,32 @@
+﻿using SharpGL.SceneGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimLab.Geometry
+{
+    /// <summary>
+    /// 三棱行的描述
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TriangularPrismPosition
+    {
+        /**
+         * 顶三角形的坐标按顺序组成一个三角形,(p1,p2),(p4,p5)构成一个四边形，（
+         * */
+        public Vertex P1;
+        public Vertex P2;
+        public Vertex P3;
+
+        /**
+         * 底三角形的坐标按顺序组成一个三角形，
+         * */
+        public Vertex P4;
+        public Vertex P5;
+        public Vertex P6;
+
+    }
+}
