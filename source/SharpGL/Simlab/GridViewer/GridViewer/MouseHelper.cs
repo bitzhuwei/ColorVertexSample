b3c9ef257b764738c335c20346278bf161b3ee5e
dalanmao <14881236@qq.com>
2016-05-24 10:16:37 +0800

    commit missing files

---
+++ source/SharpGL/Simlab/GridViewer/GridViewer/MouseHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GridViewer
+{
+    class MouseHelper
+    {
+        
+
+        public static void WaitIdle(){
+
+           Application.OpenForms[0].Cursor = Cursors.WaitCursor;
+           Application.Idle -= OnIdle;
+           Application.Idle += OnIdle;
+        }
+
+        private static void OnIdle(object sender, EventArgs e)
+        {
+           Application.OpenForms[0].Cursor = null;
+           Application.Idle -= OnIdle;
+        }
+    }
+}
