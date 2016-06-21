3aeacd5eed43254c03131cfa08a2de9f082d6d48
祝威 <bitzhuwei.cnblogs@qq.com>
2016-06-21 10:24:45 +0800

    clear

--- source/SharpGL/Simlab/SimLab/GridSources/CatesianGridderSource.cs
+++ source/SharpGL/Simlab/SimLab/GridSources/CatesianGridderSource.cs
@@ -31,7 +31,7 @@ namespace SimLab.GridSource
         public float[] DZ { get; set; }
 
 
-        public float[] TOPS {get; set;}
+        public float[] TOPS { get; set; }
 
         /// <summary>
         /// 数组大小为 (nx+1)*(ny+1)*(nz+1);
@@ -46,16 +46,21 @@ namespace SimLab.GridSource
 
         private GridIndexer coordIndexer;
 
-
         /// <summary>
         /// 网格起始原点X坐标
         /// </summary>
         public float OX { get; set; }
 
+        /// <summary>
+        /// 网格起始原点Y坐标
+        /// </summary>
         public float OY { get; set; }
 
+        /// <summary>
+        /// 网格起始原点Z坐标
+        /// </summary>
         public float OZ { get; set; }
-   
+
         /// <summary>
         /// 前左上角坐标
         /// </summary>
@@ -65,17 +70,17 @@ namespace SimLab.GridSource
         /// <returns></returns>
         public override Vertex PointFLT(int i, int j, int k)
         {
-           Vertex p = new Vertex();
-           int gridIndex = this.coordIndexer.IndexOf(i, j, k);
-           p.X= this.xcoords[gridIndex];
-           p.Y = this.ycoords[gridIndex];
-           p.Z = this.zcoords[gridIndex];
+            Vertex p = new Vertex();
+            int gridIndex = this.coordIndexer.IndexOf(i, j, k);
+            p.X = this.xcoords[gridIndex];
+            p.Y = this.ycoords[gridIndex];
+            p.Z = this.zcoords[gridIndex];
             return p;
         }
         public override Vertex PointFRT(int i, int j, int k)
         {
             Vertex p = new Vertex();
-            int gridIndex = this.coordIndexer.IndexOf(i+1,j,k);
+            int gridIndex = this.coordIndexer.IndexOf(i + 1, j, k);
             p.X = this.xcoords[gridIndex];
             p.Y = this.ycoords[gridIndex];
             p.Z = this.zcoords[gridIndex];
@@ -100,20 +105,19 @@ namespace SimLab.GridSource
             p.Z = this.zcoords[gridIndex];
             return p;
         }
+
         public override Vertex PointBLT(int i, int j, int k)
         {
-
             Vertex p = new Vertex();
             int gridIndex = this.coordIndexer.IndexOf(i, j + 1, k);
             p.X = this.xcoords[gridIndex];
             p.Y = this.ycoords[gridIndex];
             p.Z = this.zcoords[gridIndex];
             return p;
-
         }
+
         public override Vertex PointBRT(int i, int j, int k)
         {
-
             Vertex p = new Vertex();
             int gridIndex = this.coordIndexer.IndexOf(i + 1, j + 1, k);
             p.X = this.xcoords[gridIndex];
@@ -124,21 +128,16 @@ namespace SimLab.GridSource
 
         public override Vertex PointBLB(int i, int j, int k)
         {
-
             Vertex p = new Vertex();
             int gridIndex = this.coordIndexer.IndexOf(i, j + 1, k + 1);
             p.X = this.xcoords[gridIndex];
             p.Y = this.ycoords[gridIndex];
             p.Z = this.zcoords[gridIndex];
             return p;
-
-
-         
         }
 
         public override Vertex PointBRB(int i, int j, int k)
         {
-
             Vertex p = new Vertex();
             int gridIndex = this.coordIndexer.IndexOf(i + 1, j + 1, k + 1);
             p.X = this.xcoords[gridIndex];
@@ -156,89 +155,89 @@ namespace SimLab.GridSource
             {
                 this.TOPS = ArrayHelper.NewFloatArray(this.DimenSize, 0);
             }
-                //xcoords;
+            //xcoords;
             int coordSize = (this.NX + 1) * (this.NY + 1) * (this.NZ + 1);
-            float[] coordX  = new float[coordSize];
-            float[] coordY =   new float[coordSize];
-            float[] coordZ =   new float[coordSize];
+            float[] coordX = new float[coordSize];
+            float[] coordY = new float[coordSize];
+            float[] coordZ = new float[coordSize];
 
             float[] srcDX = this.DX;
             float[] srcDY = this.DY;
             float[] srcDZ = this.DZ;
             float[] tops = this.TOPS;
 
-            int cnx = this.NX+1;
-            int cny = this.NY+1;
-            int cnz = this.NZ+1;
+            int cnx = this.NX + 1;
+            int cny = this.NY + 1;
+            int cnz = this.NZ + 1;
 
             int dnx = this.NX;
             int dny = this.NY;
             int dnz = this.NZ;
 
-            GridIndexer  coordIndexer = new GridIndexer(cnx, cny, cnz);
+            GridIndexer coordIndexer = new GridIndexer(cnx, cny, cnz);
             //dx, dy,dx 描述
-            GridIndexer  dIndexer = new GridIndexer(this.NX, this.NY, this.NZ);
+            GridIndexer dIndexer = new GridIndexer(this.NX, this.NY, this.NZ);
 
-            int  coordIndex;
-            int  prevcIndex;
-            int  di,dj,dk, xGridIndex,yGridIndex,zGridIndex;
+            int coordIndex;
+            int prevcIndex;
+            int di, dj, dk, xGridIndex, yGridIndex, zGridIndex;
             for (int kcz = 1; kcz <= cnz; kcz++)
             {
                 for (int jcy = 1; jcy <= cny; jcy++)
                 {
                     for (int icx = 1; icx <= cnx; icx++)
                     {
-                         coordIndex = coordIndexer.IndexOf(icx, jcy, kcz);
+                        coordIndex = coordIndexer.IndexOf(icx, jcy, kcz);
 
                         //处理x坐标
-                         if (icx == 1)
-                         {
-                             coordX[coordIndex] = this.OX;
-                         }
-                         else
-                         {
-                              prevcIndex = coordIndexer.IndexOf(icx - 1, jcy, kcz);
-                              //距离坐标
-                              di = icx-1;
-                              dj = jcy > dny ? jcy-1: jcy;
-                              dk = kcz > dnz ? kcz-1:kcz;
-                              xGridIndex = dIndexer.IndexOf(di,dj,dk);
-                              coordX[coordIndex] = coordX[prevcIndex] + srcDX[xGridIndex];
-                         }
+                        if (icx == 1)
+                        {
+                            coordX[coordIndex] = this.OX;
+                        }
+                        else
+                        {
+                            prevcIndex = coordIndexer.IndexOf(icx - 1, jcy, kcz);
+                            //距离坐标
+                            di = icx - 1;
+                            dj = jcy > dny ? jcy - 1 : jcy;
+                            dk = kcz > dnz ? kcz - 1 : kcz;
+                            xGridIndex = dIndexer.IndexOf(di, dj, dk);
+                            coordX[coordIndex] = coordX[prevcIndex] + srcDX[xGridIndex];
+                        }
 
                         //计算(icx,jcy,kcz)网格的坐标
-                         if (jcy == 1)
-                         {
-                             coordY[coordIndex] = this.OY;
-                         }
-                         else
-                         {
-                             prevcIndex = coordIndexer.IndexOf(icx, jcy - 1, kcz);
-                             di = icx > dnx ? icx - 1 : icx;
-                             dj = jcy - 1;
-                             dk = kcz > dnz ? kcz - 1 : kcz;
-                             yGridIndex = dIndexer.IndexOf(di, dj, dk);
-                             coordY[coordIndex] = coordY[prevcIndex] + srcDY[yGridIndex];
-                         }
+                        if (jcy == 1)
+                        {
+                            coordY[coordIndex] = this.OY;
+                        }
+                        else
+                        {
+                            prevcIndex = coordIndexer.IndexOf(icx, jcy - 1, kcz);
+                            di = icx > dnx ? icx - 1 : icx;
+                            dj = jcy - 1;
+                            dk = kcz > dnz ? kcz - 1 : kcz;
+                            yGridIndex = dIndexer.IndexOf(di, dj, dk);
+                            coordY[coordIndex] = coordY[prevcIndex] + srcDY[yGridIndex];
+                        }
 
-                         if (kcz == 1)
-                         {
-                             int celli = icx > this.NX ? this.NX:icx;
-                             int cellj = jcy > this.NY ? this.NY:jcy;
-                             int cellk = kcz > this.NZ ? this.NZ:kcz;
-                             int cellIndex = dIndexer.IndexOf(celli,cellj,cellk);
-                             float topz = tops[cellIndex];
-                             coordZ[coordIndex] = this.OZ+topz;
-                         }
-                         else
-                         {
-                             prevcIndex = coordIndexer.IndexOf(icx, jcy, kcz - 1);
-                             di = icx > dnx ? dnx : icx;
-                             dj = jcy > dny ? dny : jcy;
-                             dk = kcz - 1;
-                             zGridIndex = dIndexer.IndexOf(di, dj, dk);
-                             coordZ[coordIndex] = coordZ[prevcIndex] + srcDZ[zGridIndex];
-                         }
+                        if (kcz == 1)
+                        {
+                            int celli = icx > this.NX ? this.NX : icx;
+                            int cellj = jcy > this.NY ? this.NY : jcy;
+                            int cellk = kcz > this.NZ ? this.NZ : kcz;
+                            int cellIndex = dIndexer.IndexOf(celli, cellj, cellk);
+                            float topz = tops[cellIndex];
+                            coordZ[coordIndex] = this.OZ + topz;
+                        }
+                        else
+                        {
+                            prevcIndex = coordIndexer.IndexOf(icx, jcy, kcz - 1);
+                            di = icx > dnx ? dnx : icx;
+                            dj = jcy > dny ? dny : jcy;
+                            dk = kcz - 1;
+                            zGridIndex = dIndexer.IndexOf(di, dj, dk);
+                            coordZ[coordIndex] = coordZ[prevcIndex] + srcDZ[zGridIndex];
+                        }
                     }
                 }
             }
@@ -246,11 +245,6 @@ namespace SimLab.GridSource
             this.ycoords = coordY;
             this.zcoords = coordZ;
             this.coordIndexer = coordIndexer;
-        }
-
-        public override void Init()
-        {
-             base.Init();        
         }
 
     }
